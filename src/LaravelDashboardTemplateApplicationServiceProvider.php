2ae172c501b22f813830e5b7458d05b4a18f72ae
Stratulat Alexandru <alexanderstratulat97@gmail.com>
2019-07-12 13:34:30 +0000

    Apply fixes from StyleCI

--- src/LaravelDashboardTemplateApplicationServiceProvider.php
+++ src/LaravelDashboardTemplateApplicationServiceProvider.php
@@ -24,7 +24,7 @@ class LaravelDashboardTemplateApplicationServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        foreach([
+        foreach ([
             'userName',
             'userAvatar',
             'sidebarLinks',
